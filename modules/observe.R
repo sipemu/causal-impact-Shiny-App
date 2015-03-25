3596c88f1c66520a14e660a4284f97cc4e7a03f6
Simon Müller <info@muon-stat.com>
2015-03-25 09:35:57 +0100

    update

--- modules/observe.R
+++ modules/observe.R
@@ -8,6 +8,7 @@ observe({
     start <- tsData$date[1]
     end <- tsData$date[nrow(tsData)]
     target <- tsData$date[floor(length(start:end) / 2)]
+
     
     updateDateRangeInput(session = session,
                          inputId = "dateRange",
@@ -21,4 +22,35 @@ observe({
                     value   = target)
   })
   # session$sendCustomMessage(type = 'testmessage', message ="La suma de los valores de cada nivel debe  ser igual al N hipotetico")
+})
+
+observe({
+  start <-startDateCache
+  end <- endDateCache
+  target <- start + (end - start) / 2
+  article <- global.tsCache$articles$x
+  names(article) <- as.numeric(global.tsCache$articles$x)
+  
+  
+  updateDateRangeInput(session,
+                       inputId = "dateRange",
+                       label   = "Date Range",
+                       start   = start,
+                       end     = end)
+  
+  updateDateInput(session,
+                  inputId = "eventDate",
+                  label   = "Event Date",
+                  value   = target)
+  
+  updateSelectizeInput(session,
+                       inputId  = "storeID",
+                       label    = "Store", 
+                       choices  = global.tsCache$sites$site, 
+                       selected = global.tsCache$sites$site[1])
+  updateSelectizeInput(session,
+                 inputId  = "articleID",
+                 label    = "Article", 
+                 choices  = article, 
+                 selected = article[1])
 })
--- ui.R
+++ ui.R
@@ -29,7 +29,76 @@ dashboardPage(skin = "black",
 #                                                    label   = "upload data")
 #                                       )
 #                 ),
-              uiOutput("ui_setup")
+fluidRow(
+  shinydashboard::box(title       = "Select Dates", 
+                      width       = 4,
+                      solidHeader = FALSE,
+                      status      = "info", 
+                      height      = 250, 
+                      dateRangeInput(inputId = "dateRange",
+                                     label   = "Date Range",
+                                     start   = Sys.Date() - 10,
+                                     end     = Sys.Date()),
+                      helpText("Choose date range of data. Include lead time before and after event date for sensible results."),
+                      actionButton("getDataButton", "Get Data")
+  ),
+  shinydashboard::box(title       = "Select Store & Article", 
+                      width       = 4,
+                      solidHeader = FALSE,
+                      status      = "info", 
+                      height      = 250, 
+                      selectizeInput(inputId  = "storeID",
+                                     label    = "Store", 
+                                     choices  = "empty", 
+                                     selected = NA, 
+                                     multiple = F),
+                      selectizeInput(inputId  = "articleID",
+                                     label    = "Article", 
+                                     choices  = "empty", 
+                                     selected = NA, 
+                                     multiple = F)
+  ),
+  shinydashboard::box(title       = "Measurement", 
+                      width       = 4,
+                      solidHeader = FALSE,
+                      status      = "info", 
+                      height      = 250, 
+                      radioButtons(inputId  = "measVariable",
+                                   label    = NA, 
+                                   choices  = c("Sales" = "SUM", "Volume" = "COUNT"),
+                                   selected = "SUM"),
+                      helpText("Choose measurement: Sales: sum of sales; Volume: count of sales")
+  )
+),
+fluidRow(
+  shinydashboard::box(title       = "Click-Drag to Zoom. Double-Click to Reset. Shift-Drag to Pan.", 
+                      width       = 12,
+                      dygraphOutput("tsPlot")
+  )
+),
+fluidRow(
+  shinydashboard::box(title       = "When did the event happend?", 
+                      width       = 4,
+                      solidHeader = FALSE,
+                      status      = "warning", 
+                      height      = 250, 
+                      dateInput(inputId = "eventDate",
+                                label   = "Event Date",
+                                value   = Sys.Date() - 5),
+                      helpText("This value/date is what will be tested to see if it had a statistically significant impact.")
+  ),
+  shinydashboard::box(title       = "Does the Data Have Seasonality?", 
+                      width       = 4,
+                      solidHeader = FALSE,
+                      status      = "info", 
+                      height      = 250, 
+                      radioButtons(inputId  = "season",
+                                   label    = "Seasonality",
+                                   choices  = c("None" = 1, "Weekly" = 7, "Monthly" = 31),
+                                   selected = 1),
+                      helpText("This value/date is what will be tested to see if it had a statistically significant impact.")
+  )
+)
               ),
       tabItem("tab_results", 
               uiOutput("ui_results")
